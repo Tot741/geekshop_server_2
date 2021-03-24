62c00c48892e1fae007de7e049f8a978fa8e0e87
Oleg Upovalov <2987026@mail.ru>
2021-03-25 01:21:19 +0300

    fixes after the code review of lesson2

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -2,8 +2,6 @@ from basket.models import Basket
 
 
 def basket_count(request):
-    print(f'context processor basket works')
-
     if request.user.is_authenticated:
         baskets = Basket.objects.filter(user=request.user)
         return {'basketcount': sum(basket.quantity for basket in baskets)}
